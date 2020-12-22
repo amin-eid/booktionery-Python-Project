ebfe84748c7c9e0b4bcb1dfadf95d6fd4887e27a
abdqam <abood_q93@hotmail.com>
2020-12-22 10:44:11 +0200

    updated 10:43am 22/12/2020 by abd

--- booktioneryapp/views.py
+++ booktioneryapp/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render, HttpResponse
 def index(request):
-    return HttpResponse("Welcome to Booktionery project (Amin,Abdusalam,Mais)!")
+    return render(request,'index.html')
