eda8110fc309761da60e1e006e128d898a5e7733
abdqam <abood_q93@hotmail.com>
2020-12-23 10:29:59 +0200

    updated at 10:29 23-12-2020

--- booktioneryapp/views.py
+++ booktioneryapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render, HttpResponse
 def index(request):
-    return render(request,'index.html')
+    return render(request,'main.html')
+
+
+def register(request):
+    return render(request,'register.html')
