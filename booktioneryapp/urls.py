136bf957781b0fd15f8abfeeb07061f214481ba1
Amin Eid <amin.eid1982@yahoo.com>
2020-12-23 11:11:15 +0200

    updated 23/12/2020 12:23 by Amin

--- booktioneryapp/urls.py
+++ booktioneryapp/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.index),	
     path('register', views.register),
+    path('user', views.user),
+    path('thanks', views.thanks),
+    path('admin2', views.admin2),
+    path('admin3', views.admin3),
+    path('admin4', views.admin4),
 ]
--- booktioneryapp/views.py
+++ booktioneryapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, HttpResponse
+
 def index(request):
     return render(request,'index.html')
 
-from django.shortcuts import render, HttpResponse
 def register(request):
     return render(request,'register.html')
-    
+
+def user(request):
+    return render(request,'user.html')
+
+def thanks(request):
+    return render(request,'thankyou.html')
+
+def admin2(request):
+    return render(request,'adminorders.html')
+def admin3(request):
+    return render(request,'adminusers.html')
+def admin4(request):
+    return render(request,'adminproducts.html')
