921d9934dd579767ea53dcee0ad3596a9ddc7f4a
abdqam <abood_q93@hotmail.com>
2020-12-23 11:07:21 +0200

    updated 23-12-2020 11:07 by abd

--- booktioneryapp/views.py
+++ booktioneryapp/views.py
@@ -5,3 +5,20 @@ def index(request):
 
 def register(request):
     return render(request,'register.html')
+
+def login(request):
+    if request.method=='POST':
+        errors = models.login_errors(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            user = models.login(request.POST)
+            if user is not None:
+                if 'user_id' not in request.session:
+                    request.session['user_id'] = user.id
+                    request.session['first_name'] = user.first_name
+                    request.session['last_name'] = user.last_name
+                    return redirect('/quotes')
+    return redirect('/')
